e2b7ce011bf3337b054d6898190dad9f4841aaa9
Salvatore Filippone <sfilippone@gmail.com>
2025-12-29 15:14:29 -0800

    adding hero theme control

--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -3,7 +3,17 @@
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-            <section class="hero">Hero</section>
+            <section class="hero" style="background: url('https://my-site.local/wp-content/uploads/2025/12/pexels-lumn-167699-scaled.jpeg');">
+                <div class="overlay" style="min-height: 800px;">
+                    <div class="container">
+                        <div class="hero-items">
+                            <h1>Lorem ipsum dolor</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada dolor vitae nulla rhoncus, et aliquet nisl consequat. Quisque ipsum ligula, vestibulum a faucibus eu, lacinia sit amet tellus</p>
+                            <a href="#">Learn more </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <section class="services">
                 <h2>Services</h2>
                 <div class="container">
