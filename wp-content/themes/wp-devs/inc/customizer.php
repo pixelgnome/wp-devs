eef53bb07e757ebb872effce8e8bb71384bc48af
Salvatore Filippone <sfilippone@gmail.com>
2025-12-30 09:29:19 -0800

    adding more params to theme customizer

--- wp-content/themes/wp-devs/inc/customizer.php
+++ wp-content/themes/wp-devs/inc/customizer.php
@@ -29,6 +29,115 @@ function wpdevs_customizer($wp_customize)
             'type' => 'text'
         )
     );
+
+
+    // 2 Hero
+    $wp_customize->add_section(
+        'sec_hero',
+        array(
+            'title' => 'Hero Section'
+        )
+    );
+
+    // 2 Hero -> Title
+    $wp_customize->add_setting(
+        'set_hero_title',
+        array(
+            'type' => 'theme_mod',
+            'default' => 'Set your hero title',
+            'sanitize_callback' => 'sanitize_text_field'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_hero_title',
+        array(
+            'label' => 'Hero Title',
+            'description' => 'Type your hero text here',
+            'section' => 'sec_hero',
+            'type' => 'text'
+        )
+    );
+
+    // 3 Hero -> SubTitle
+    $wp_customize->add_setting(
+        'set_hero_subtitle',
+        array(
+            'type' => 'theme_mod',
+            'default' => 'Set your hero subtitle',
+            'sanitize_callback' => 'sanitize_textarea_field'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_hero_subtitle',
+        array(
+            'label' => 'Hero Subtitle',
+            'description' => 'Type your hero subtitle here',
+            'section' => 'sec_hero',
+            'type' => 'textarea'
+        )
+    );
+
+    // 3 Hero -> Button Text
+    $wp_customize->add_setting(
+        'set_hero_button_text',
+        array(
+            'type' => 'theme_mod',
+            'default' => 'Set your hero button text',
+            'sanitize_callback' => 'sanitize_text_field'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_hero_button_text',
+        array(
+            'label' => 'Hero Button Text',
+            'description' => 'Type your hero button text',
+            'section' => 'sec_hero',
+            'type' => 'text'
+        )
+    );
+
+    // 3 Hero -> Button Link
+    $wp_customize->add_setting(
+        'set_hero_button_link',
+        array(
+            'type' => 'theme_mod',
+            'default' => '#',
+            'sanitize_callback' => 'esc_url_raw'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_hero_button_link',
+        array(
+            'label' => 'Hero Button Link',
+            'description' => 'Choose where button will link',
+            'section' => 'sec_hero',
+            'type' => 'url'
+        )
+    );
+
+    // 3 Hero min height
+    $wp_customize->add_setting(
+        'set_hero_height',
+        array(
+            'type' => 'theme_mod',
+            'default' => 800,
+            'sanitize_callback' => 'absint'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_hero_height',
+        array(
+            'label' => 'Hero minimum height',
+            'description' => 'Set the min-height of the hero section',
+            'section' => 'sec_hero',
+            'type' => 'number'
+        )
+    );
 }
 
 add_action('customize_register', 'wpdevs_customizer');
